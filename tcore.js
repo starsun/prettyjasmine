adc315f38716bf7f4a7d81739848b4ab8657a020
bencode <bencode@163.com>
2012-02-19 21:35:50 +0800

    优化运行

--- tcore.js
+++ tcore.js
@@ -1,60 +1,83 @@
-/**
- * 测试公用方法
- * @author qijun.weiqj
- */
 (function() {
- 
-var extend = function(des, src) {
-	for (var k in src || {}) {
-		des[k] = src[k];
-	}
-};
 
+var $ = jQuery;
+window.$ = window.jQuery = undefined;
 
-var globalCache = {},
-	cacheField = 'tcoreconfig' + (new Date()).getTime();
-
-var config = function(name, value) {
-	if (Object.prototype.toString.call(name) === '[object Object]') {
-		for (var k in name) {
-			config(k, name[k]);
-		}
-		return;
-	}
-
-	var o = arguments.callee.caller,
-		cache = null;
-	
-	if (value !== undefined) { // set
-		cache = o ? o[cacheField] : globalCache;
-		if (!cache) {
-			cache = o[cacheField] = {};  
-		}
-		cache[name] = value;
-		return value;
-	}
-	
-	// get
-	while (o) {
-		cache = o[cacheField];
-		if (cache && cache[name] !== null) {
-			break;  
-		}
-		o = o.caller;
-	}
-	return (cache || globalCache)[name];
-}
-
-
-var require = function(url) {
-		
-};
-
-
-extend(window, {
-	config: config,
-	require: require
-});
-
+var TCore = {
+	init: function() {
+		this._proxyDescribe();
+	},
   
+	run: function() {
+		var self = this;
+		this._loadSpec(function() {
+			self._loadRequires(function() {
+				self._runDescribe();  
+				self._runJasmine();
+			});
+		});
+	},
+
+	require: function(src) {
+		this._requires.push(src);
+	},
+
+	_loadSpec: function(callback) {
+		var url = /^\?(.*)$/.exec(window.location.search)[1];
+		$.ajax(url, { 
+			dataType: 'script', 
+			cache: false,
+			success: callback
+		});
+	},
+
+	_proxyDescribe: function() {
+		var self = this;
+		this._describe = window.describe;		 
+		window.describe = function() {
+			self._describes.push(arguments);		 
+		};
+	},
+
+	_loadRequires: function(callback) {
+		var defer = $.when({});
+		$.each(this._requires, function(index, require) {
+			defer = defer.pipe(function() {
+			  return $.ajax(require, { dataType: 'script', cache: false });
+			});
+		});
+		defer.done(callback);
+	},
+
+	_runDescribe: function() {
+		window.describe = this._describe;
+		$.each(this._describes, function(index, desc) {
+			window.describe.apply(window, desc);	
+		});
+	},
+
+	_runJasmine: function() {
+		var reporter = new jasmine.TrivialReporter(),
+			env = jasmine.getEnv();
+
+		env.updateInterval = 1000;
+		env.addReporter(reporter);
+		env.specFilter = function(spec) {
+			return reporter.specFilter(spec);
+		};
+
+		env.execute();
+	},
+
+	_requires: [],
+	_describes: []
+
+};
+	
+
+$.extend(window, TCore);
+TCore.init();
+$($.proxy(TCore, 'run'));
+  
+
 })();
