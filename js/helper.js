c0382d996b33ff5621f6bfea142a9eb70b0b821b
bencode <bencode@163.com>
2012-02-27 21:36:53 +0800

    完成

---
+++ js/helper.js
@@ -0,0 +1,41 @@
+/**
+ * ¸¨Öú¹¤¾ßÀà
+ * @author qijun.weiqj@alibaba-inc.com
+ */
+define(['lib/jquery'], function($) {
+
+return {
+	join: function() {
+		var url = arguments[0] || '';
+		for (var i = 1, len = arguments.length; i < len; i++) {
+			var right =	arguments[i] || '';
+			url = url.replace(/\/+$/, '') + '/' + right.replace(/^\/+/, '');
+		}
+		return url;
+	},
+
+	unparam: function(search) {
+		var map = {};
+		search = (search || '').replace(/^\?/, '').split('&');
+		$.each(search, function(index, part) {
+			part = part.split('=', 2);
+			map[$.trim(part[0])] = $.trim(part[1] || '');
+		});
+		return map;
+	},
+
+	url: function(url, search) {
+		return url + (url.indexOf('?') === -1 ? '?' : '&') + search;
+	},
+
+	serial: function(fns) {
+		var promise = $.when();	
+		$.each(fns, function(i, fn) {
+			promise = promise.pipe(fn);	
+		});
+		return promise;
+	}
+};
+
+		
+});
